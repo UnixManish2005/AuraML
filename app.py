eb0a29c74f51ea8cef5c44bbe7d55dca8efca6e4
Manishankar Dey <manishankardey2005@gmail.com>
2026-05-28 10:27:53 +0530

    Emoji changed

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ def show_sidebar(user):
         st.markdown(f"""
         <div style="background:#1A1A2E;border-radius:12px;padding:.7rem 1rem;
                     border:1px solid rgba(108,99,255,0.2);margin-bottom:1rem;text-align:center">
-            <div style="font-size:1.6rem">🙈</div>
+            <div style="font-size:1.6rem">🙋🏻‍♀️</div>
             <div style="font-weight:700;color:#E8E8F0;font-size:.9rem">{user['name']}</div>
             <div style="color:#8888AA;font-size:.75rem">{user['email']}</div>
             <span style="background:rgba(108,99,255,.2);color:#6C63FF;
